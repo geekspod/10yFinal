4297aef3df0b333908d55973585596202a323502
MZO9400 <hamza.hameed00@yahoo.com>
2021-02-21 18:16:44 +0500

    Remove unwanted files

--- ci/wordpress/wp-content/plugins/creative-mail-by-constant-contact/assets/js/footer_rating.js
+++
@@ -1,4 +0,0 @@
-jQuery( 'a.ce4wp-rating-link' ).click( function() {
-    jQuery.post( 'admin-ajax.php', { action: 'woocommerce_ce4wp_rated' } );
-    jQuery( this ).parent().text( jQuery( this ).data( 'rated' ) );
-});
